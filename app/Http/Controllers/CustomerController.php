e39696e33cfc26e5bab4f40f101b3d0e606e5b4f
Webpixal <nazmul.rookz@gmail.com>
2024-11-23 01:41:43 +0600

    1st API get

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    //
+    function addCustomer(){
+        return "Customer Fuction";
+    }
+
+    function Customers(){
+        return "Customer get Fuction";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomerController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Customers', [CustomerController::class,'Customers']);
+Route::post('/add-customer', [CustomerController::class,'Customer']);
